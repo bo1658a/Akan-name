04bb70c2b6ee73493c29aa3baeaa75bed9593f99
Bob Okoth <bob.okoth@student.moringaschool.com>
2021-12-13 14:31:54 +0300

    adding link

---
+++ index.js
@@ -0,0 +1,10 @@
+function check_dayofbirth(){
+
+var dayofbirth = document.getElementById("date").value;
+ 
+if((dayofbirth == sunday && (male == Kwasi)|| (female == Akosua))) {
+alert(dayofbirthSunday +  "isKwasi")
+} else {
+    alert(dayofbirthSunday + "femaleAkosua");
+}
+}
